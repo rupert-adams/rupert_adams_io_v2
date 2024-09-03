289cbad5bbe9ffd34d3d7e3bf26ce0ab78dd9567
Rupert Adams <RupertNCAdams@aol.com>
2024-09-03 19:33:53 +0200

    updating contact and MANSION work

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Contact = () => (
     <div className="App-component">
         <div className="App-contact">
-            <p className="App-contact-para">Feel free to contact me via e-mail, follow me on twitter or add me on LinkedIn. On second thought, its probably better not to follow me on twitter.</p>
+            <p className="App-contact-para">Feel free to contact me via e-mail, follow me on Bluesky or add me on LinkedIn. Alternatively if you can find one I also accept messenger pigeons.</p>
             <div className="App-contact-links">
                 <a href="mailto:rupertadams@protonmail.com" className="Link-text">
                     <h1 className="Link-text-big">Email</h1>
                     <p className="Link-text-small">rupertadams@protonmail.com</p>
                 </a>
-                <a href="https://www.twitter.com/rupert_adams" className="Link-text" target="_blank">
-                    <h1 className="Link-text-big">Twitter</h1>
-                    <p className="Link-text-small">@rupert_adams</p>
+                <a href="https://bsky.app/profile/rupertadams.bsky.social" className="Link-text" target="_blank">
+                    <h1 className="Link-text-big">Bluesky</h1>
+                    <p className="Link-text-small">@rupertadams.bsky.social</p>
                 </a>
                 <a href="https://www.linkedin.com/in/rupertncadams/" className="Link-text" target="_blank">
                     <h1 className="Link-text-big">LinkedIn</h1>
